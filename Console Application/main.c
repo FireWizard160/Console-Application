d2ea23aa2d1c75abf5e541538523060ba2821dd0
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-09-15 16:45:06 +0200

    UE3

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< Updated upstream
-int main()
-{
-    printf("Hello world!\n");
-=======
+
 int main(){
     float s;
     scanf("%f",&s);
     printf("With tax: %.2f",s*1.2);
 
 
->>>>>>> Stashed changes
     return 0;
 }
