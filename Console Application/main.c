5564b08fe7185359ee515642db300d3d6c0633d0
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-09-15 16:19:21 +0200

    UE2

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main(){
+    int s;
+    scanf("%d",&s);
+    printf(": ");
+    printf("Surface area: %d\n",6*s*s);
+    printf("Volume: %d",s*s*s);
+
     return 0;
+
+
+
+
+
+
 }
