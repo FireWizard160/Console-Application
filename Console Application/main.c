a9ff7b3990adce5ca133b3e79916647321cb8c3e
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-09-18 22:34:40 +0200

    UE4

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+int main() {
+    char number1[1], number2[1], number3[1];
 
-int main(){
-    float s;
-    scanf("%f",&s);
-    printf("With tax: %.2f",s*1.2);
+    scanf("%c", &number1);
+    scanf(" %c", &number2);
+    scanf("  %c", &number3); /* The leading whitespace ensures it's the
+                          previous newline is ignored */
+    printf("number1: %c, number2: %c, number3: %c\n", number1, number2, number3);
+
 
 
     return 0;
