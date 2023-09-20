1586a0dbed0ca9769b122301f382c93b58987461
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-09-20 20:12:22 +0200

    UE4 FINISH

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int dig1, dig2, dig3,octazahl, z1,z2,z3;
+    int dezimal, d1, d2, d3 , octazahl, z1, z2, z3;
 
-    scanf("%1d%1d%1d", &dig1, &dig2, &dig3);
-
-
-    if(dig1<8&&dig2<8&&dig3<8)
-     octazahl=dig1 * 8 * 8 + dig2 * 8 + dig3 * 1;
-    printf(": dec.:%d\n", octazahl);
-
-    z1=octazahl%10;
-    octazahl/=10;
-    z2=octazahl%10;
-    octazahl/=10;
-    z3=octazahl%10;
+    scanf("%d", &dezimal);
+    d1 = dezimal % 10;
+    dezimal /= 10;
+    d2 = dezimal % 10;
+    dezimal /= 10;
+    d3 = dezimal % 10;
 
 
-    printf("ZS: %d",z1+z2+z3);
+
+    if (d1 < 8 && d2 < 8 && d3 < 8)
+    {
+        octazahl = d3 * 8 * 8 + d2 * 8 + d1 * 1;
+        printf(": dec.:%d\n", octazahl);
+
+        z1 = octazahl % 10;
+        octazahl /= 10;
+        z2 = octazahl % 10;
+        octazahl /= 10;
+        z3 = octazahl % 10;
 
 
+        printf("ZS: %d", z1 + z2 + z3);
+    }
 
     return 0;
 }
