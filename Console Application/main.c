153a275adfa6c4b91d4e3d66b44dc182f2d6ae1b
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-09-26 17:24:23 +0200

    UE8

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,50 +1,24 @@
 #include <stdio.h>
 
 int main() {
-int noten, noten2, n1, n2, n3;
+
+int days, start, i;
 
 
-    scanf("%d", &noten);
-    noten2 = noten;
+printf("number of days in month: ");
+scanf("%d",&days);
+printf("starting day of week (1=Monday, 7=Sunday): \n");
+scanf("%d",&start);
 
-printf("Noten: ");
+    for (i = 1; i <= days; i++, start++) {
 
-if(noten > 999 || noten < 111 ){
-    printf("Erwarte genau 3 Noten!\n");
-return 0;
+        printf("%2d ", i);
+        if (start == 7) {
+        printf("\n");
+        start = start - 7;
+        }
     }
-
-    n1 = noten % 10;
-    noten /= 10;
-    n2 = noten % 10;
-    noten /= 10;
-    n3 = noten % 10;
-
-
-if (n3 > 3 || n3 < 1) {
-    printf("Erste Note muss zwischen 1 und 5 liegen!\n");
-    return 0;
-}
-
-if (n2 > 5 || n2 < 1){
-    printf("Zweite Note muss zwischen 1 und 5 liegen!\n");
-    return 0;
-}
-if (n1 > 5 || n1 < 1){
-    printf("Dritte Note muss zwischen 1 und 5 liegen!\n");
-    return 0;
-}
-
-if ( n1 == 5 || n2 == 5 || n3 == 5 )
-    printf("Nicht bestanden!\n");
-
-else if ( n1 == 1 && n2 == 1 && n3 == 1 )
-    printf("Ausgezeichneter Erfolg!\n");
-
-else if ( noten2 == 211 || noten2 == 121 || noten2 == 112 )
-    printf("Guter Erfolg!\n");
-
-else printf("Bestanden.\n");
+printf("\n");
 
 return 0;
 }
