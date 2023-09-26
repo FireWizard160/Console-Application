c12fa6370fe0f421e4c8f88a96d1f159c279b3da
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-09-26 19:20:36 +0200

    UE8 Finish

--- Console Application/main.c
+++ Console Application/main.c
@@ -2,31 +2,31 @@
 
 int main() {
 
-int days, start, i;
+    int days, start, i,j;
 
 
-printf("number of days in month: ");
-scanf("%d",&days);
-printf("starting day of week (1=Monday, 7=Sunday): \n");
-scanf("%d",&start);
+    printf("number of days in month: ");
+    scanf("%d",&days);
+    printf("starting day of week (1=Monday, 7=Sunday): ");
+    scanf("%d",&start);
+    printf("\n");
+    for (j = 1; j < start ;j++) {
+        printf("   ");
+
+    }
 
     for (i = 1; i <= days; i++, start++) {
 
         printf("%2d ", i);
         if (start == 7) {
-        printf("\n");
-        start = start - 7;
+            printf("\n");
+            start = start - 7;
         }
     }
-printf("\n");
+    printf("\n");
 
-return 0;
+    return 0;
 }
-
-
-
-
-
 
 
 
