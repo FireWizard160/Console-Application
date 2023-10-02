affc4e6ef5fb24238bf1b3a1d7ea833b20daa461
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-10-02 12:44:49 +0200

    Zwischentest 1 Save 1

--- Console Application/main.c
+++ Console Application/main.c
@@ -3,45 +3,76 @@
 
 
 int main() {
-    double min = DBL_MAX, max = DBL_MIN, avg, wert, sum;
-    int cnt = 0;
+    int eingabe, cntE, cntA;
+    double guthaben = 100, brutto, netto, steuer, steuerges, steuerfrei, avgA, avgE, betrag;
 
 
     do {
 
+        printf("Typ:");
+        scanf("%.2lf", &eingabe);
+        printf("Betrag in Euro: ");
+        scanf("2.lf", &betrag);
 
-        printf(": ");
-        scanf("%lf.2", &wert);
-        if (wert <= 0) {
+        if (eingabe < 0)
+            printf("Der Betrag kann nicht negativ sein");
 
 
-            break;
+
+        switch (eingabe) {
+            case 'e':
+
+                if (betrag <= 20)
+                    guthaben = +betrag;
+                if (betrag > 20 && betrag <= 50)
+                    steuer = (betrag - 20) * 0.1;
+                steuerges = +steuer;
+                guthaben = +eingabe;
+
+                if (eingabe > 50 && eingabe <= 200)
+                    guthaben = +20;
+                steuer = (eingabe - 200) * 0.2;
+
+                steuerges = +steuer;
+                steuer = (eingabe - 20) * 0.1;
+                steuerges = +steuer;
+                guthaben = +eingabe;
+
+                if (eingabe > 200)
+                    steuer = (eingabe - 50) * 0.2;
+
+                steuerges = +steuer;
+                steuer = (eingabe - 20) * 0.1;
+                steuerges = +steuer;
+                guthaben = +eingabe;
+
+
+                cntE++;
+
+                break;
+            case 's':
+                guthaben = +betrag;
+
+
+                cntE++;
+                break;
+            case 'k':
+                if (eingabe > guthaben)
+                    printf("Diese Kosten können nicht bezahlt werden");
+
+
+                cntA++;
+                break;
+            case '=':
+
+
+                break;
         }
 
-        if (wert < min)
-            min = wert;
-        if (wert > max)
-            max = wert;
+    } while (eingabe == '=');
 
-
-        sum += wert;
-        cnt++;
-
-
-    } while (wert > 0);
-
-
-    avg = sum / cnt;
-
-    if (cnt == 0) {
-        printf("no number entered");
-        return 0;
-    }
-
-    printf("cnt: %d\n", cnt);
-    printf("min: %.2lf\n", min);
-    printf("max: %.2lf\n", max);
-    printf("avg: %.2lf\n", avg);
+    printf("%d Einnahmen durchschnittlichem Wert %d Euro", cntE, avgE);
+    printf("%d Einnahmen durchschnittlichem Wert %d Euro", cntA, avgA);
 
 
     return 0;
