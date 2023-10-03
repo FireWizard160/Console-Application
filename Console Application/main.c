c8d3bbbc83b8a89ee8f3376776a8d929f2d9ed7d
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-10-03 20:02:04 +0200

    Zwischentest 1 Save 3

--- Console Application/main.c
+++ Console Application/main.c
@@ -3,89 +3,132 @@
 
 
 int main() {
-    int eingabe, cntE, cntA;
-    double guthaben = 100, brutto, netto, steuer, steuerges, steuerfrei, avgA, avgE, betrag, ages, eges;
-
+    char eingabe;
+    int cntE = 0, cntA = 0;
+    double guthaben = 100, steuer, avgA = 0, avgE = 0, betrag, ages, eges, steuerges, zsm;
+    printf("\n");
 
     do {
+        printf("Guthaben: %.2f Euro", guthaben);
+        printf("\n");
+        printf("Typ: ");
+        printf("\n");
 
-        printf("Typ:");
-        scanf("%d", &eingabe);
+        scanf(" %c", &eingabe);
+        if (eingabe == '=')
+            break;
+
 
         if (eingabe != '=') {
-            printf("Betrag in Euro:\n");
-            scanf("2.lf", &betrag);
+            printf("Betrag in Euro: ");
+            printf("\n");
+            scanf(" %lf", &betrag);
         }
         if (betrag < 0) {
-            printf("Der Betrag kann nicht negativ sein");
+            printf("Der Betrag kann nicht negativ sein.");
+            printf("\n");
+            continue;
         }
 
 
         switch (eingabe) {
             case 'e':
 
-                if (betrag <= 20) {
-                    guthaben = +betrag;
+                if (betrag <= 20.00) {
+                    guthaben = guthaben + betrag;
+                    printf("Gezahlte Steuern: %.2f Euro", steuer);
+                    printf("\n");
                 }
 
-                if (betrag > 20 && betrag <= 50) {
-                    steuer = (betrag - 20) * 0.1;
-                    steuerges = +steuer;
-                    guthaben = +betrag;
+                if (betrag > 20.00 && betrag <= 50.00) {
+                    zsm = betrag;
+                    steuer = (zsm - 20) * 0.1;
+                    steuerges += steuer;
+                    betrag = betrag - steuer;
+                    guthaben += betrag;
+                    printf("Gezahlte Steuern: %.2f Euro", steuer);
+                    printf("\n");
                 }
 
-                if (betrag > 50 && betrag <= 200) {
-                    guthaben = +20;
-                    steuer = (betrag - 70) * 0.2;
-                    steuer =+ 30*0.1;
+                if (betrag > 50.00 && betrag <= 200.00) {
+                    zsm = betrag;
+                    steuer = ((zsm - 50) * 0.2) + (30 * 0.1);
+                    betrag = betrag - steuer;
+                    guthaben = guthaben + betrag;
+                    steuerges = steuerges + steuer;
 
-                    steuerges = +steuer;
-                    guthaben = +betrag;
+                    printf("Gezahlte Steuern: %.2f Euro", steuer);
+                    printf("\n");
                 }
-                if (eingabe > 200) {
-                    guthaben = +20;
-                    steuer = +30 * 0.1;
-                    steuer = +130 * 0.2;
-                    steuer =+ (betrag-130)*0.4;
+                if (betrag > 200) {
+                    zsm = betrag;
+                    steuer = ((zsm - 200) * 0.4) + (150 * 0.2) + (30 * 0.1);
+                    betrag = betrag - steuer;
+                    guthaben = guthaben + betrag;
+                    steuerges += steuer;
 
+                    printf("Gezahlte Steuern: %.2f Euro", steuer);
+                    printf("\n");
 
                 }
-                    eges = eges + eingabe - steuer;
-                    cntE++;
+                eges = eges + betrag;
+                cntE++;
 
                 break;
             case 's':
 
 
-                guthaben = +betrag;
-                printf("Guthaben: %d",guthaben);
+                guthaben = guthaben + betrag;
 
-                avgE = avgE + eingabe;
+
+                eges = eges + betrag;
                 cntE++;
                 break;
+
             case 'k':
-                if (betrag > guthaben)
-                    printf("Diese Kosten können nicht bezahlt werden");
-
-
-                ages = ages + eingabe;
+                if (betrag > guthaben) {
+                    printf("Diese Kosten koennen nicht bezahlt werden.");
+                    printf("\n");
+                    continue;
+                }
+                guthaben = guthaben - betrag;
+                ages = ages + betrag;
                 cntA++;
                 break;
+
             case '=':
 
-
+            // der case breakt das
                 break;
         }
 
-    } while (eingabe == '=');
+    } while (eingabe != '=');
 
+    printf("Restguthaben: %.2f Euro", guthaben);
+    printf("\n");
 
-    avgE=eges/cntE;
-    avgA=ages/cntA;
+    if (cntE == 0) {
+        printf("%d Einnahmen mit durchschnittlichem Wert %.2lf Euro", cntE, avgE);
+        printf("\n");
+    }
 
+    else {
+        avgE = eges / cntE;
+        printf("%d Einnahmen mit durchschnittlichem Wert %.2lf Euro", cntE, avgE);
+        printf("\n");
+    }
+    if (cntA == 0) {
+        printf("%d Ausgaben mit durchschnittlichem Wert %.2lf Euro", cntA, avgA);
+        printf("\n");
+    }
+    else {
+        avgA = ages / cntA;
+        printf("%d Ausgaben mit durchschnittlichem Wert %.2lf Euro", cntA, avgA);
+        printf("\n");
+    }
 
-    printf("%d Einnahmen durchschnittlichem Wert %d Euro\n", cntE, avgE);
-    printf("%d Einnahmen durchschnittlichem Wert %d Euro\n", cntA, avgA);
+    printf("Gezahlte Steuern: %.2f Euro", steuerges);
+    printf("\n");
 
 
     return 0;
