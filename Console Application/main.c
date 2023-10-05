995be09b17f081475f7f9c35e346b069c6c7c3bd
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-10-05 20:21:02 +0200

    UE10 Finish

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
-#include <math.h>
-int x,y;
-
-int hoch(int x,int y) {
-
-        return pow(x,y);
-
-
-
-
-}
 
 
 
 int main() {
-    scanf("%d", &x);
-    scanf("%d", &y);
+
+    int position = 1, leader = 1, eingabe = 1, startnummer = 1;
+
+    while (eingabe != 0 ) {
+
+        printf(": ");
+        scanf("%d", &eingabe);
+        if (eingabe == 0)
+            break;
+        startnummer++;
+        if (eingabe <= position) {
+            position++;
+            printf("position #1: %d\n",position);
+        }
+
+
+        if (eingabe == 1) {
+            leader = startnummer;
+            printf("new leader: #%d\n", leader);
+        }
 
 
 
 
 
-    printf("%d\n",hoch(x,y));
-
+    }
+    printf("final position #1: %d\n",position);
+    printf("winner: #%d\n",leader);
 
     return 0;
 }
