079adabb07b989e1af428bbd516ac8fe255ce875
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-11-05 19:51:57 +0100

    Selbstcheck Heap

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+char* mirrorString(char *string)
+{
+
+
+    int stringLength = strlen(string);
+
+    char* mirroredString = (char*)malloc(sizeof(char) * stringLength + 1);
+    strcpy(mirroredString, string);
+    char temp [stringLength];
+
+    int n = 0;
+    for (int i = stringLength - 1; i >= 0 ; --i, n++) {
+
+        temp [n] = string[i];
+
+    }
+    n = 0;
+
+    for (int i = stringLength/2; i < stringLength ; ++i, n++) {
+        mirroredString [i] = temp [i];
+
+    }
+
+
+    return mirroredString;
+}
 int main()
 {
-    printf("Hello world!\n");
+    char string[30];
+    scanf("%s", string);
+
+    char* stringMirrored = mirrorString(string);
+
+    printf("%s", stringMirrored);
+
+    free (stringMirrored);
     return 0;
 }
