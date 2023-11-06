26fdc4ac9aae2ff20b19198bc254b7e5033fbc4f
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-11-06 12:40:45 +0100

    Zwischentest 2 save 1

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,8 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+
+struct Struktur{
+
+    int enum;
+    int time;
+    int duration;
+
+
+
+};
+
+
+
+
+void printAppointments(struct appointment *schedule, int length) {
+}
+
+void printInformation(struct appointment *schedule, int length) {
+
+}
+
+struct appointment createAppointment() {
+
+
+
+
+}
+
+void addAppointment(struct appointment *schedule, int *length) {
+
+
+}
+
+void deleteAppointment(struct appointment *schedule, int *length) {
+
+
+}
+
+void menue() {
+
+
+    printf("Neuen Termin (n), Termin loeschen (d), Termine auflisten (l), Informationen ausgeben (i), Programm beenden (x)\n")
+    printf("Auswahl: ")
+
+}
+
+
+int main() {
+
+    switch (menue()) {
+
+        case 'n':
+            printf();
+            break;
+
+        case 'n':
+            printf();
+            break;
+
+        case 'n':
+            printf();
+            break;
+
+        case 'n':
+            printf();
+            break;
+
+        case 'x':
+            return 0;
+
+
+    }
+
+
 }
