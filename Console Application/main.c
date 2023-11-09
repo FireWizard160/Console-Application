3a46d6e44eeca9a1854a03b80a9e290ba22e1724
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-11-09 22:09:39 +0100

    Verkettete Listen I

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,8 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct node {
+    int element;
+    struct node* next;
+};
 
+
+
+
+struct node* addBefore(int value, struct node* head)
+{
+    struct node* newHead =
+            (struct node*)malloc(sizeof(struct node));
+    newHead->element = value;
+    newHead->next = head;
+    return newHead;
+
+}
+
+void size(struct node* head)
+{
+
+    struct node* temp = head;
+    while(temp != NULL)
+    {
+        printf("%d ",temp->element);
+        temp = temp->next;
+    }
+
+}
+
+void freeList(struct node* head) {
+    struct node* temp = head;
+    while (head != NULL) {
+        head = head->next;
+        free(temp);
+        temp = head;
+    }
+}
 int main()
 {
-    printf("Hello world!\n");
+    struct node* head = NULL;
+int value;
+
+
+    do {
+
+
+        scanf("%d",&value);
+        if (value != 0)
+            head = addBefore(value, head);
+
+    }while (value != 0);
+
+
+
+
+    size(head);
+    freeList(head);
     return 0;
 }
