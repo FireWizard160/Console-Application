561a6f018d26d5983f6bb006191407f9fc0e0e09
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-11-30 18:06:42 +0100

    Vekettete Listen I besser

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,62 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct node {
-    int element;
-    struct node* next;
-};
+    struct node *next;
+    int value;
+} typedef node;
 
+node *createNode(int value) {
+    node *newNode = (node *) malloc(sizeof(node));
+    newNode->value = value;
+    newNode->next = NULL;
+    return newNode;
+}
 
+node *addBefore(node* newNode, node* list){
+     newNode->next = list;
+    return newNode;
+}
 
+void freeMemory(node *list){
+    if (list == NULL)
+        return;
+    freeMemory(list->next);
 
-struct node* addBefore(int value, struct node* head)
-{
-    struct node* newHead =
-            (struct node*)malloc(sizeof(struct node));
-    newHead->element = value;
-    newHead->next = head;
-    return newHead;
+    free(list);
 
 }
 
-void size(struct node* head)
-{
+int main() {
+    node *list = NULL;
+    int value = 0;
 
-    struct node* temp = head;
-    while(temp != NULL)
-    {
-        printf("%d ",temp->element);
-        temp = temp->next;
-    }
-
-}
-
-void freeList(struct node* head) {
-    struct node* temp = head;
-    while (head != NULL) {
-        head = head->next;
-        free(temp);
-        temp = head;
-    }
-}
-int main()
-{
-    struct node* head = NULL;
-int value;
-
-
-    do {
-
-
+    while (1){
         scanf("%d",&value);
-        if (value != 0)
-            head = addBefore(value, head);
+        if(value == 0)
+            break;
 
-    }while (value != 0);
+        list = addBefore(createNode(value),list);
+    }
 
+    for (node* n = list; n != NULL; n = n->next) {
+        printf("%d ",n->value);
 
+    }
+    freeMemory(list);
 
-
-    size(head);
-    freeList(head);
     return 0;
 }
