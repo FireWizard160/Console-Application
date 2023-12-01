bbeb93af8a1b2efbe463d1ed2afb7d7b11eb9fc8
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-12-01 17:56:13 +0100

    UE18 finish

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+int main(){
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+    char string[41];
+    string[40] = '\0';
+
+    char temp;
+    int count = 0;
+    printf(": ");
+
+    for (int i = 0; i < 40;) {
+        scanf("%c", &temp);
+
+
+        string[i] = tolower(temp);
+
+
+
+        if (string[i] == '\n') {
+            string[i] = '\0';
+            break;
+        }
+
+        if (isalpha(string[i])) {
+            i++;
+            count++;
+        }
+
+
+    }
+
+
+
+    for (int i = 0, j = count - 1; i < count - 1; ++i, --j) {
+        if (string[j] == '\0'){
+            j--;
+            j--;
+            continue;
+        }
+
+
+
+
+
+
+        if (string[i] != string[j]){
+            printf("false");
+            return 0;
+
+        }
+
+
+    }
+    printf("true");
 }
