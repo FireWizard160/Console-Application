9a192fbda44a95566a5f68553d84ab2fee268b28
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-12-01 17:02:40 +0100

    UE20 finish

--- Console Application/main.c
+++ Console Application/main.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 
 
-int main()
-{
+int main() {
     int arraySize1 = 0;
     int arraySize2 = 0;
-    printf(":");
+    printf(": ");
 
     scanf("%d", &arraySize1);
     scanf("%d", &arraySize2);
 
-    printf(":");
+
 
     int array1[arraySize1];
     int array2[arraySize2];
@@ -23,125 +22,44 @@ int main()
     }
 
 
-
     for (int i = 0, temp = 0; i < arraySize2; ++i) {
         scanf("%d", &temp);
         array2[i] = temp;
 
     }
 
-    int numberOfDiffs = 0;
+    int diff = 0;
     int smallestDiff = 0;
     int smallestDiffArray1 = 0;
     int smallestDiffArray2 = 0;
 
-    numberOfDiffs = (arraySize1 * arraySize2) * 2;
+    smallestDiff = array1[0] - array2 [0];
 
-    int arrayDiffs[numberOfDiffs];
-    int i = 0;
-    for (int j = 0,n = 0; i < numberOfDiffs; ++i) {
+    if (smallestDiff < 0) {
+        smallestDiff *= -1;
+    }
 
-        if (n == arraySize1 - 1)
-            if(arraySize1 != 1){
-                break;
+    for (int i = 0; i < arraySize1; ++i) {
+        for (int j = 0; j < arraySize2; ++j) {
+            diff = array1[i] - array2[j];
+
+            if (diff < 0) {
+                diff *= -1;
+            }
+
+            if (diff < smallestDiff) {
+                smallestDiff = diff;
+                smallestDiffArray1 = i;
+                smallestDiffArray2 = j;
             }
 
 
-
-        arrayDiffs[i] = array1[n] - array2[j];
-
-
-
-
-        if (i == 0){
-            if (arrayDiffs[0] < 0){
-                j++;
-                if ( j == arraySize2){
-                    n++;
-                    j = 0;
-                }
-                continue;
-            }
-            smallestDiff = arrayDiffs [i];
-            smallestDiffArray1 = array1[n];
-            smallestDiffArray2 = array2[j];
-            j++;
-            if ( j == arraySize2){
-                n++;
-                j = 0;
-            }
-            continue;
-        }
-
-
-        if (arrayDiffs[i] > - 1 && arrayDiffs [i - 1] < 0) {
-            smallestDiff = arrayDiffs [i];
-            smallestDiffArray1 = array1[n];
-            smallestDiffArray2 = array2[j];
-
-        } else if (arrayDiffs[i] > - 1 && arrayDiffs[i] < smallestDiff){
-            smallestDiff = arrayDiffs [i];
-            smallestDiffArray1 = array1[n];
-            smallestDiffArray2 = array2[j];
-        }
-        j++;
-        if ( j == arraySize2){
-            n++;
-            j = 0;
         }
 
     }
 
-    for (int j = 0,n = 0; i < numberOfDiffs; ++i) {
 
-        if (n == arraySize1 - 1)
-            if(arraySize1 != 1){
-                break;
-            }
-
-        arrayDiffs[i] = array2[n] - array1[j];
-
-
-
-        if (i == 0){
-            if (arrayDiffs[0] < 0){
-                j++;
-                if ( j == arraySize2){
-                    n++;
-                    j = 0;
-                }
-                continue;
-
-            }
-            smallestDiff = arrayDiffs [i];
-            smallestDiffArray1 = array1[j];
-            smallestDiffArray2 = array2[n];
-            j++;
-            if ( j == arraySize2){
-                n++;
-                j = 0;
-            }
-            continue;
-        }
-        if (arrayDiffs[i] > - 1 && arrayDiffs [i - 1] < 0){
-            smallestDiff = arrayDiffs [i];
-            smallestDiffArray1 = array1[j];
-            smallestDiffArray2 = array2[n];
-
-        } else if (arrayDiffs[i] > - 1 && arrayDiffs[i] < smallestDiff){
-            smallestDiff = arrayDiffs [i];
-            smallestDiffArray1 = array1[j];
-            smallestDiffArray2 = array2[n];
-        }
-
-        j++;
-        if (j == arraySize2){
-            n++;
-            j = 0;
-        }
-    }
-
-    printf("%d (%d, %d)", smallestDiff, smallestDiffArray1, smallestDiffArray2);
+    printf("%d (%d, %d)", smallestDiff, array1[smallestDiffArray1], array2[smallestDiffArray2]);
 
     return 0;
 }
