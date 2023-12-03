46d2dea0b157c8fbb65ebc22e44408a6a5a24dc9
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-12-04 00:05:38 +0100

    Zwischentest 3 save 1

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+struct node {
+    struct node *next;
+    int value;
+} typedef node;
+
+node *createNode(int value) {
+    node *newNode = (node *) malloc(sizeof(node));
+    newNode->value = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+node *addAfter(node *newNode, node *list) {
+
+
+    for (node *n = list; n != NULL; n = n->next) {
+        if (n->next == NULL) {
+            n->next = newNode;
+            break;
+        }
+
+
+    }
+
+
+    return newNode;
+}
+
+void freeMemory(node *list) {
+    if (list == NULL)
+        return;
+    freeMemory(list->next);
+
+    free(list);
+
+}
+
+void printList(node *list) {
+
+    for (node *n = list; n != NULL; n = n->next) {
+        printf("%d ", n->value);
+
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
 int main()
 {
     printf("Hello world!\n");
