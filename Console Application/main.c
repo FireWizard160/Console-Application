af025e22ee9141ed34162cfe9b184110c15a6142
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-12-04 00:57:12 +0100

    Zwischentest 3 save 2

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum genre {
+    horror = 'h',
+    adventure = 'a',
+    romance = 'r',
+    nonfiction = 'n',
+} typedef genre;
 
-struct node {
-    struct node *next;
-    int value;
-} typedef node;
+struct book {
+    char title[32];
+    enum genre;
+    int year;
+    int amount;
+};
 
-node *createNode(int value) {
-    node *newNode = (node *) malloc(sizeof(node));
-    newNode->value = value;
+
+struct bookNode {
+    struct bookNode *next;
+    struct book;
+} typedef bookNode;
+
+struct borrowNode {
+    struct borrowNode *next;
+    char title[32];
+    char name[32];
+} typedef borrowNode;
+
+bookNode *createBookNode(int value) {
+    bookNode *newNode = (bookNode *) malloc(sizeof(bookNode));
+    //newNode->book = value;
     newNode->next = NULL;
     return newNode;
 }
 
-node *addAfter(node *newNode, node *list) {
+bookNode *addBefore(bookNode *newNode, bookNode *list) {
 
 
-    for (node *n = list; n != NULL; n = n->next) {
+    for (bookNode *n = list; n != NULL; n = n->next) {
         if (n->next == NULL) {
             n->next = newNode;
             break;
@@ -30,7 +50,7 @@ node *addAfter(node *newNode, node *list) {
     return newNode;
 }
 
-void freeMemory(node *list) {
+void freeMemory(bookNode *list) {
     if (list == NULL)
         return;
     freeMemory(list->next);
@@ -39,23 +59,140 @@ void freeMemory(node *list) {
 
 }
 
-void printList(node *list) {
 
-    for (node *n = list; n != NULL; n = n->next) {
-        printf("%d ", n->value);
+void printUngueltigeEingabe(){
+    printf("\nUngueltige Eingabe!");
+}
+
+bookNode *insertBook() {
+    struct book newBook;
+    char newTitle[32];
+    int input = 0;
+    char charInput;
+
+    while (1){
+        printf("\nGeben Sie den Titel ein");
+
+        for (int i = 0; i < 32; ++i) {
+            scanf("%c", &charInput);
+            newBook.title[i] = charInput;
+        }
+        break;
+
+    }
+
+    while (1){
+        printf("\nGeben Sie das Genre ein. Horror (1), Abenteuer (2), Romantik (3), Sachbuch (4):");
+        scanf("%d",&input);
+        if(input < 1 || input > 4){
+            printUngueltigeEingabe();
+            continue;
+        }
+        newBook.
+        break;
+    }
+
+    while (1){
+        printf("\nGeben Sie das Erscheinungsjahr ein:");
+        scanf("%d",&input);
+
+        if(input > 0){
+            printUngueltigeEingabe();
+            continue;
+        }
+
+        break;
+    }
+
+    while (1){
+        printf("\nGeben Sie ein wieviele Exemplare vorhanden sind: ");
+        scanf("%d",&input);
+
+        if(input > 0){
+            printUngueltigeEingabe();
+            continue;
+        }
+
+        break;
+    }
+
+}
+
+void borrowBook() {
+
+}
+
+void returnBook() {
+
+}
+
+/*void printList() {
+    for (bookNode *n = list; n != NULL; n = n->next) {
+        printf("%d ", n->book);
 
     }
     printf("\n");
+
+}
+*/
+void printListInOrder() {
+
 }
 
+int main() {
+    bookNode* inventoryList;
+
+
+    char input;
+
+    while (1){
+        printf("\nNeues Buch eingeben (n), Buch ausleihen (b), Buch zurueckgeben (r), Buecher auflisten (l), Buecher sortieren (s), Programm beenden (x)");
+        printf("\nAuswahl: ");
+        scanf(" %c",&input);
+
+        switch (input) {
+            case 'n':
+                insertBook();
+
+                break;
+
+            case 'b':
+                borrowBook();
+
+
+                break;
+
+            case 'r':
+                returnBook();
+
+                break;
+
+            case 'l':
+                //printList();
+
+                break;
+
+            case 's':
+                printListInOrder();
+
+                break;
+
+            case 'x':
+                return 0;
+
+                break;
+
+            default:
+                printUngueltigeEingabe();
+
+                break;
+
+        }
 
 
 
+    }
 
 
-
-int main()
-{
-    printf("Hello world!\n");
     return 0;
 }
