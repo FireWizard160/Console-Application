2b384c73a0934b8e9d708018ae51f77ba1ab7d9f
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2023-12-04 02:02:40 +0100

    Zwischentest 3 save 4

--- Console Application/main.c
+++ Console Application/main.c
@@ -13,7 +13,7 @@ struct book {
     int genre;
     int year;
     int amount;
-}typedef book;
+} typedef book;
 
 
 struct bookNode {
@@ -28,19 +28,15 @@ struct borrowNode {
 } typedef borrowNode;
 
 bookNode *createBookNode(book newBook) {
-    bookNode *newNode = (bookNode *) malloc(sizeof(bookNode));
-    newNode->book = newBook;
-    newNode->next = NULL;
-    return newNode;
+    bookNode *newBookNode = (bookNode *) malloc(sizeof(bookNode));
+    newBookNode->book = newBook;
+    newBookNode->next = NULL;
+    return newBookNode;
 }
 
-bookNode *addBefore(bookNode *newNode, bookNode *inventorylist) {
-
-
-
-
-
-    return newNode;
+bookNode *addBefore(bookNode *newBookNode, bookNode *inventoryList) {
+    newBookNode->next = inventoryList;
+    return newBookNode;
 }
 
 void freeMemory(bookNode *list) {
@@ -52,32 +48,42 @@ void freeMemory(bookNode *list) {
 
 }
 
+char getString (int inventoryList){
+    
 
-void printUngueltigeEingabe(){
+}
+
+void printUngueltigeEingabe() {
     printf("\nUngueltige Eingabe!");
 }
 
-bookNode *insertBook() {
+book insertBook() {
     struct book newBook;
-    char newTitle[32];
+    newBook.title[31] = '\0';
     int input = 0;
     char charInput;
 
-    while (1){
-        printf("\nGeben Sie den Titel ein");
+    while (1) {
+        printf("\nGeben Sie den Titel ein: ");
 
         for (int i = 0; i < 32; ++i) {
-            scanf("%c", &charInput);
-            newBook.title[i] = charInput;
+            scanf("%c", &newBook.title[i]);
+            if (newBook.title[i] == '\n' && i != 0){
+                newBook.title[i] = '\0';
+                break;
+            }
+
         }
+
+
         break;
 
     }
 
-    while (1){
+    while (1) {
         printf("\nGeben Sie das Genre ein. Horror (1), Abenteuer (2), Romantik (3), Sachbuch (4):");
-        scanf("%d",&input);
-        if(input < 1 || input > 4){
+        scanf("%d", &input);
+        if (input < 1 || input > 4) {
             printUngueltigeEingabe();
             continue;
         }
@@ -85,11 +91,11 @@ bookNode *insertBook() {
         break;
     }
 
-    while (1){
+    while (1) {
         printf("\nGeben Sie das Erscheinungsjahr ein:");
-        scanf("%d",&input);
+        scanf("%d", &input);
 
-        if(input > 0){
+        if (input < 0) {
             printUngueltigeEingabe();
             continue;
         }
@@ -97,18 +103,19 @@ bookNode *insertBook() {
         break;
     }
 
-    while (1){
+    while (1) {
         printf("\nGeben Sie ein wieviele Exemplare vorhanden sind: ");
-        scanf("%d",&input);
+        scanf("%d", &input);
 
-        if(input > 0){
+        if (input < 0) {
             printUngueltigeEingabe();
             continue;
         }
         newBook.amount = input;
         break;
     }
-    createBookNode()
+    return newBook;
+
 }
 
 void borrowBook() {
@@ -119,33 +126,44 @@ void returnBook() {
 
 }
 
-/*void printList() {
-    for (bookNode *n = list; n != NULL; n = n->next) {
-        printf("%d ", n->book);
+void printList(bookNode inventoryList) {
+    int i = 0;
+    for (bookNode *n = &inventoryList; n != NULL; n = n->next) {
+        i++;
+
+        printf("\n%d: ", i);
+        for (int j = 0; j < 32; ++j) {
+            if (n->book.title[j] == '\0')
+                break;
+
+            printf(" %c", n->book.title[j]);
+
+        }
+        printf(", &d (%d) ", getString(n->book.genre), n->book.year);
 
     }
-    printf("\n");
+
 
 }
-*/
+
 void printListInOrder() {
 
 }
 
 int main() {
-    bookNode* inventoryList;
+    struct bookNode *inventoryList = NULL;
 
 
     char input;
 
-    while (1){
+    while (1) {
         printf("\nNeues Buch eingeben (n), Buch ausleihen (b), Buch zurueckgeben (r), Buecher auflisten (l), Buecher sortieren (s), Programm beenden (x)");
         printf("\nAuswahl: ");
-        scanf(" %c",&input);
+        scanf(" %c", &input);
 
         switch (input) {
             case 'n':
-                insertBook();
+                inventoryList = addBefore(createBookNode(insertBook()), inventoryList);
 
                 break;
 
@@ -161,7 +179,7 @@ int main() {
                 break;
 
             case 'l':
-                //printList();
+                printList(*inventoryList);
 
                 break;
 
@@ -171,9 +189,9 @@ int main() {
                 break;
 
             case 'x':
+                freeMemory(inventoryList);
                 return 0;
 
-                break;
 
             default:
                 printUngueltigeEingabe();
@@ -181,7 +199,6 @@ int main() {
                 break;
 
         }
-
 
 
     }
