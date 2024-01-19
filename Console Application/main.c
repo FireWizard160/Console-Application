4b5fa19fb759e02cf70156d9a4888c8f8edaa19c
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2024-01-19 16:48:56 +0100

    UE25 finish

--- Console Application/main.c
+++ Console Application/main.c
@@ -1,70 +1,212 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <malloc.h>
 
 struct node {
-    int element;
-    struct node* next;
-};
 
-struct node* addBefore(int value, struct node* head)
-{
-    struct node* newHead =
-            (struct node*)malloc(sizeof(struct node));
-    newHead->element = value;
-    newHead->next = head;
-    return newHead;
+struct  node* next;
+char name;
+
+
+}typedef node;
+
+
+node * createNode(char name){
+
+ node* newNode = (struct node*)malloc(sizeof(struct node));
+ newNode->name = name;
+ newNode->next = NULL;
+
+return newNode;
+}
+
+node * addNodeAfter(struct node* newNode, struct node* list){
+if (list == NULL){
+    newNode->next = NULL;
+    return newNode;
+}
+node* tmp = list;
+
+for(;tmp->next != NULL; tmp = tmp->next){
 
 }
 
+tmp->next = newNode;
+return list;
+}
 
 
+void printList(node* list){
 
-void size(struct node* head)
-{
+    node *temp = list;
+        while(temp != NULL)
+        {
 
-    struct node* temp = head;
-    while(temp != NULL)
-    {
-        printf("%d ",temp->element);
+            printf("%c ",temp->name);
         temp = temp->next;
-    }
+        }
 
 }
 
-void freeList(struct node* head) {
-    struct node* temp = head;
-    while (head != NULL) {
-        head = head->next;
-        free(temp);
-        temp = head;
+int cntList(node * list){
+    int cnt = 0;
+
+    if (list == NULL){
+        return cnt;
     }
+
+    if (list->next == NULL){
+        cnt++;
+        return cnt;
+    }
+   node* tmp = list;
+   for(;tmp != NULL; tmp = tmp->next){
+    cnt++;
+}
+    return cnt;
 }
 
+
+
+
+
+void freeList(node *list) {
+    if (list == NULL)
+        return;
+    freeList(list->next);
+
+    free(list);
+
+}
 
 
 int main()
 {
+	// Lists here
+	struct node* person = NULL;
+	struct node* kassa1 = NULL;
+	struct node* kassa2 = NULL;
+	struct node* temp = NULL;
 
-        struct node* head = NULL;
-        int value;
-
-
-        do {
-
-
-            scanf("%d",&value);
-            if (value != 0)
-                head = addBefore(value, head);
-
-        }while (value != 0);
+    int cntPerson = 0;
+    int cntKassa1 = 0;
+    int cntKassa2 = 0;
 
 
+    while(42)
+    {
+        char n;
+
+        printf(": ");
+        scanf(" %c", &n);
+
+        if(n == '-') break;
+
+        // Add person
+        person = addNodeAfter(createNode(n), person);
+        cntPerson++;
+
+    }
 
 
-        size(head);
-        freeList(head);
-        return 0;
+
+    int run  = 1;
+    while(run)
+    {
+        // Print here
+        printf("Personen: %d\n",cntPerson);
+
+        printList(person);
 
 
+
+        printf("\nKassa 1: %d\n",cntKassa1);
+
+        printList(kassa1);
+
+        printf("\nKassa 2: %d\n",cntKassa2);
+
+        printList(kassa2);
+
+
+
+        char c;
+        printf("\n: ");
+        scanf(" %c", &c);
+
+
+        if(c == '-') break;
+
+
+
+        switch(c)
+        {
+        case '-':
+            run = 0;
+            break;
+        case 'a':
+            // anstellen
+
+
+           if(cntKassa1 == cntKassa2 || cntKassa1 < cntKassa2){
+            temp = person;
+
+            person = person->next;
+            temp->next = NULL;
+            kassa1 = addNodeAfter(temp, kassa1);
+           }
+
+           if (cntKassa2 < cntKassa1){
+
+            temp = person;
+
+            person = person->next;
+            temp->next = NULL;
+            kassa2 = addNodeAfter(temp, kassa2);
+
+           }
+
+           cntPerson--;
+            cntKassa1 = cntList(kassa1);
+            cntKassa2 = cntList(kassa2);
+            break;
+        case '1':
+            // kassa1
+
+            temp = kassa1;
+
+            kassa1 = kassa1->next;
+
+
+
+            free(temp);
+            cntKassa1 = cntList(kassa1);
+            break;
+        case '2':
+            // kassa2
+
+
+            temp = kassa2;
+
+            kassa2 = kassa2->next;
+
+
+
+            free(temp);
+            cntKassa2 = cntList(kassa2);
+            break;
+        }
+    }
+
+    // free list
+
+    freeList(person);
+    freeList(kassa1);
+    freeList(kassa2);
+
+    return 0;
 }
+
+
+
